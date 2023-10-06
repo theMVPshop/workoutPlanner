ee05e3bf0d0b7b9f84271c68a385c1ffe1843778
Vic <vicjs612@gmail.com>
2023-10-05 20:54:05 -0500

    update homepage

--- sweat-smart/src/components/HomePage/HomePage.js
+++ sweat-smart/src/components/HomePage/HomePage.js
@@ -1,24 +1,54 @@
 import React from "react";
 
+import { images } from "../../constants";
 import "./homepage.css";
-import LOGO1 from "../../assets/LOGO1.png";
 
 const HomePage = () => {
   return (
-    <div className="section__padding" data-aos="fade-down">
-      <div className="heading__feature">
-        <div className="heading__feature_logo">
-          <img src={LOGO1} alt="logo" />
-          <h1>Sweat Smart</h1>
+    <div className="section__padding header" data-aos="fade-down">
+      <div className="heading__logo">
+        <img src={images.LOGO1} alt="logo" />
+        <div className="slogan">
+          <h1>
+            Welcome to SweatSmart,
+            <br />
+            where fitness meets intelligence.{" "}
+          </h1>
         </div>
-        <div className="heading__feature_text">
-          <p>
-            "Welcome to Sweat Smart, where fitness meets intelligence. Our
-            AI-powered workout plan generator tailors each session to your
-            unique goals and abilities, helping you unlock your full potential,
-            one smart sweat at a time. Get ready to transform your fitness
-            journey with precision, guidance, and results like never before."
-          </p>
+      </div>
+      <div className="heading__content">
+        <div className="heading__content-one">
+          <div className="heading__content-one_img">
+            <img src={images.excercise} alt="excercise img" />
+          </div>
+          <div className="heading__content_text">
+            <p>
+              Our AI-powered workout plan generator tailors each session to your
+              unique goals and abilities.
+            </p>
+            <p>
+              Unlock your full potential, one smart sweat at a time. Get ready
+              to transform your fitness journey with precision, guidance, and
+              results like never before.
+            </p>
+          </div>
+        </div>
+        <div className="heading__content-two">
+          <div className="heading__content_text">
+            <p>
+              Let's Sweatsmart schedules workouts tailored to your needs, making
+              it easier than ever to maintain your fitness, even with a busy
+              lifestyle.
+            </p>
+          </div>
+          <div className="heading__content-two">
+            <img src={images.clock} />
+          </div>
+        </div>
+        <div className="heading__content-button">
+          <a>
+            <span>Get Started</span>
+          </a>
         </div>
       </div>
     </div>
---
+++ sweat-smart/src/constants/imges.js
@@ -0,0 +1,9 @@
+import LOGO1 from "../assets/LOGO1.png";
+import clock from "../assets/clock.png";
+import excercise from "../assets/exercise_02.png";
+
+export default {
+  LOGO1,
+  clock,
+  excercise,
+};
